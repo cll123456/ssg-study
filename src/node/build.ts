336152054e08c26136263f4335a51fcb18129769
twinkle <2642137848@qq.com>
2023-01-12 14:43:46 +0800

    feat: ✨ 完成服务端渲染

--- src/node/build.ts
+++ src/node/build.ts
@@ -2,6 +2,7 @@ import { build as viteBuild, InlineConfig } from 'vite'
 import { CLIENT_SRC_PATH, SERVER_SRC_PATH } from './constants'
 import type { RollupOutput } from 'rollup'
 import * as path from 'path'
+import * as fs from 'fs-extra'
 
 /**
  * 打包bundle
@@ -19,6 +20,7 @@ export async function buildBundle(root: string) {
             mode: 'production',
             root,
             build: {
+                ssr: isSsr,
                 outDir: isSsr ? '.temp' : 'build',
                 rollupOptions: {
                     input: isSsr ? SERVER_SRC_PATH : CLIENT_SRC_PATH,
@@ -54,6 +56,35 @@ export async function buildBundle(root: string) {
     }
 }
 
+export async function renderPage(render: () => string, root: string, clientBundle: RollupOutput) {
+    // 获取客户端打包出来的js的能力
+    const clientBundleFileName = clientBundle.output.find(chunk => chunk.type === 'chunk' && chunk.isEntry).fileName
+    const appHtml = render()
+    // 写入的html内容
+    let content = `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+           ${appHtml}
+           <script type="module" src="${clientBundleFileName}"></script>
+        </body>
+    </html>
+    `.trim()
+    // 判断build目录存在
+
+    const res = await fs.ensureDir(path.join(root, 'build'))
+    debugger
+    // 写入到build目录
+    await fs.writeFile(path.join(root, 'build', 'index.html'), content)
+    // 删除 .temp文件
+    await fs.remove(path.join(root, '.temp'))
+}
+
 /**
  * cli 打包
  * @param root
@@ -64,11 +95,7 @@ export async function build(root: string = process.cwd()) {
 
     // 2. 引入server-entry 模块
     const serverBundlePath = serverBundle.output.find(chunk => chunk.type === 'chunk' && chunk.name === 'server-entry').fileName
-    console.log(serverBundlePath, '-----serverBundlePath')
-    console.log(path.join(root, '.temp', serverBundlePath), '--------path')
-
-    const render = require(path.resolve(root, '.temp', serverBundlePath))
-
-    console.log(render)
+    const { render } = require(path.resolve(root, '.temp', serverBundlePath))
     // 3. 产出html写入磁盘
-} /*  */
+    await renderPage(render, root, clientBundle)
+}
